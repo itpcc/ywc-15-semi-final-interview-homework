54ba777fa1476479532df0ea65fdb92c3c490bc8
Rachasak Ragkamnerd <id513128@gmail.com>
2017-11-26 09:03:33 +0700

    First revision: Add modals :)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
 	var major = ['content', 'design', 'marketing', 'programming'];
+	var hilightRowClass = 'indigo darken-4 white-text';
+
 	for(majorKey in major){
 		$(`#ywc-result-section_${major[majorKey]} .ywc-result-content`).append($(".template-section .ywc-result-table").clone());
 	}
+	$('.modal').modal();
 
 	$.getJSON("https://ywc15.ywc.in.th/api/interview", function(data){
 		for(key in data){
@@ -23,8 +26,25 @@ $(document).ready(function(){
 
 		var searchValue = $.trim($("#ywc-search-text").val());
 		var searchElem = $(`.ywc-result-section .ywc-result-table .ywc-list-item:contains(${searchValue})`);
+		$('.ywc-result-section .ywc-result-table .ywc-list-item').removeClass(hilightRowClass);
+
 		if(searchElem.length){
+			var interviewRef = searchElem.find('ywc-list-item-key').text();
 			$('.ywc-result .collapsible').collapsible('open', major.indexOf(searchElem.data('major')));
+			$('#ywc-event-link').attr("href", `https://www.google.com/calendar/render?action=TEMPLATE&text=YWC%2315+interview+(Code ${interviewRef})&location=CP+Tower%2C+Si+Lom%2C+Bangkok&dates=20171126T023600Z`);
+			$('.ywc-interview-code').text(interviewRef);
+
+			searchElem.addClass(hilightRowClass);
+			var $container = $(window);
+			$container.animate({
+			    scrollTop: searchElem.offset().top - $container.offset().top + $container.scrollTop()
+			});​
+
+			$('#modal-not_found').modal('close');
+			$('#modal-congreat').modal('open');
+		}else{
+			$('#modal-not_found').modal('open');
+			$('#modal-congreat').modal('close');
 		}
 
 	});
